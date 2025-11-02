9bd93bd0f0929a364c67c18ebe2e0235155f4046
Felipe Coutinho Castilho <castfelipe10@gmail.com>
2025-11-02 16:28:45 -0300

    estudos

---
+++ array.py
@@ -0,0 +1,7 @@
+livros = ["Torre negra", "O cemitério"]
+
+#adiciona um item no final da lista
+#livros.append("Joyland")
+livros.pop[0]
+
+print(livros) 
--- condicoes.py
+++ condicoes.py
@@ -1,8 +1,8 @@
 salario = float (input ("Digite seu salario: "))
 
-if salario <= 2000
+if salario <= 2000:
     print ("Junior")
-elif salario > 2000 and salario <= 4000
+elif salario > 2000 and salario <= 4000:
     print ("Pleno")
-elif salario > 4000
-    print ()
+elif salario > 4000:
+    print ("Senior")
