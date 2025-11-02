21f5a05b918786323f8ac495158ab39c8c2db870
Felipe Coutinho Castilho <castfelipe10@gmail.com>
2025-11-02 15:18:20 -0300

    first commit

---
+++ condicoes.py
@@ -0,0 +1,8 @@
+salario = float (input ("Digite seu salario: "))
+
+if salario <= 2000
+    print ("Junior")
+elif salario > 2000 and salario <= 4000
+    print ("Pleno")
+elif salario > 4000
+    print ()
